e3c7521784fbce359d2cbf52ea027a86cf0c48a4
Christie22 <christielaurent22@lilo.org>
2019-02-19 22:43:50 +0100

    reviewed version for metrics NRR and SDR

--- libs/updated_utils.py
+++ libs/updated_utils.py
@@ -187,180 +187,215 @@ def plot_spectrogram(spect,title_name):
     plt.colorbar(format='%+2.0f dB')
     plt.title(title_name)
     plt.savefig('outputs/spectrogram_'+title_name+'.png')
-    
-def plot_manifold(n, latent_dim_vals, decoder):
-    fig, ax = plt.subplots(n, n, tight_layout=True, figsize=(20,20))
-    z_sample = latent_dim_vals
-    x = z_sample[0,0]
-    y = z_sample[0,1]
-    grid_x = norm.ppf(np.linspace(x - 0.5, x + 0.5, n))
-    grid_y = norm.ppf(np.linspace(y - 0.5, y + 0.5, n))
-    print('Figure shape : ', ax.shape)
-    for i, yi in enumerate(grid_x):
-        for j, xi in enumerate(grid_y):
-            z_sample[0,0] = xi
-            z_sample[0,1] = yi
-            #print(z_sample.shape)
-            x_decoded = decoder.predict(z_sample)
-            #print(x_decoded)
-            x_decoded = unprocess_data(x_decoded[0])
-            #np.set_printoptions(threshold=np.nan)
-            img = librosa.amplitude_to_db(np.abs(x_decoded))
-            x_spec = librosa.display.specshow(img, y_axis='log', ax=ax[i,j])
-            print(x_spec)
-            ax[i,j] = x_spec
-
-    fig.savefig('outputs/manifold.png')
-
-# Calculate all features
-def calc_features_rand(dataset_params, specs):
-    # for now it generates just random features
-    L = len(specs)
-    features = {
-        'harmon':[],
-        'sth':[],
-        'what':[]
-        }
-    for i in features:
-        features[i].extend(np.random.rand(L))
-
-    return features
 
 
-def calc_features(dataset_params, input_spectogram):
-    n_fft = dataset_params['n_fft']
-    hop_length = dataset_params['hop_length']
-    samplerate = dataset_params['samplerate']
-    centroids = []
-    centroids_dev = []
-    contrasts = []
-    flatnesses = []
-    zeroCrossings = []
-    mfccs = []
-    onset_strengths = []
-
-    for spec in input_spectogram:
-        #compute magnitude spectrum
-        y = unprocess_data(spec)
-        magSpec = np.abs(y)
-        t = librosa.core.istft(y, hop_length=hop_length, win_length=n_fft)
-
-        spectral_centroid = ftr.spectral_centroid(S=magSpec, sr=samplerate, n_fft=n_fft, hop_length=hop_length)
-        spectral_contrast = ftr.spectral_contrast(S=magSpec, sr=samplerate, n_fft=n_fft, hop_length=hop_length)
-        spectral_flatness = ftr.spectral_flatness(S=magSpec, n_fft=n_fft, hop_length=hop_length)
-        spectral_flatness = 10*np.log10(spectral_flatness) #spectral flatness in dB
-        zero_crossing_rate = ftr.zero_crossing_rate(y=t)
-        mfcc = ftr.mfcc(y=t, sr=samplerate)
-        onset_strength = onst.onset_strength(y=t, sr=samplerate) #in case we want to test it with drums
-
-        centroids.append(np.mean(spectral_centroid))
-        centroids_dev.append(np.mean(np.std(spectral_centroid)))
-        contrasts.append(spectral_contrast)
-        flatnesses.append(np.mean(spectral_flatness)) #in dB
-        zeroCrossings.append(np.mean(zero_crossing_rate))
-        mfccs.append(mfcc)
-        onset_strengths.append(onset_strength)
-
-        
-    output = {'centroid_frequency' : centroids, 'centroid_frequency_dev' : centroids_dev, 'spectral_contrasts' : contrasts, 'spectral_flatness' : flatnesses, 'zero_crossing_rate' : zeroCrossings, 'MFCC' : mfccs, 'onset_strength' : onset_strengths}
-    return output
+#def calc_features(dataset_params, input_spectogram):
+#    n_fft = dataset_params['n_fft']
+#    hop_length = dataset_params['hop_length']
+#    samplerate = dataset_params['samplerate']
+#    centroids = []
+#    centroids_dev = []
+#    contrasts = []
+#    flatnesses = []
+#    zeroCrossings = []
+#    mfccs = []
+#    onset_strengths = []
+#
+#    for spec in input_spectogram:
+#        #compute magnitude spectrum
+#        y = unprocess_data(spec)
+#        magSpec = np.abs(y)
+#        t = librosa.core.istft(y, hop_length=hop_length, win_length=n_fft)
+#
+#        spectral_centroid = ftr.spectral_centroid(S=magSpec, sr=samplerate, n_fft=n_fft, hop_length=hop_length)
+#        spectral_contrast = ftr.spectral_contrast(S=magSpec, sr=samplerate, n_fft=n_fft, hop_length=hop_length)
+#        spectral_flatness = ftr.spectral_flatness(S=magSpec, n_fft=n_fft, hop_length=hop_length)
+#        spectral_flatness = 10*np.log10(spectral_flatness) #spectral flatness in dB
+#        zero_crossing_rate = ftr.zero_crossing_rate(y=t)
+#        mfcc = ftr.mfcc(y=t, sr=samplerate)
+#        onset_strength = onst.onset_strength(y=t, sr=samplerate) #in case we want to test it with drums
+#
+#        centroids.append(np.mean(spectral_centroid))
+#        centroids_dev.append(np.mean(np.std(spectral_centroid)))
+#        contrasts.append(spectral_contrast)
+#        flatnesses.append(np.mean(spectral_flatness)) #in dB
+#        zeroCrossings.append(np.mean(zero_crossing_rate))
+#        mfccs.append(mfcc)
+#        onset_strengths.append(onset_strength)
+#
+#        
+#    output = {'centroid_frequency' : centroids, 'centroid_frequency_dev' : centroids_dev, 'spectral_contrasts' : contrasts, 'spectral_flatness' : flatnesses, 'zero_crossing_rate' : zeroCrossings, 'MFCC' : mfccs, 'onset_strength' : onset_strengths}
+#    return output
+#
 
 
 
-
-def calc_metrics(params, yest, y):
+def calc_metrics(y, yest, **kwargs):
     # calc SDR and NRR
     "SDR: ref: C. H. Taal, R. C. Hendriks, R. Heusdens, and J. Jensen, 'An Algorithm for Intelligibility Prediction of Time-Frequency Weighted Noisy Speech,' IEEE/ACM"
     
-    beta = -15 #lower SDR bound
-  
-    # evaluation parametres
-    tBands= params['tbands']
-    fBands= params['fbands']
-    norm_yest = params['norm_yest']
-    clip_yest= params['clip_yest']
+    # checking the type of the input
+    sizY = y.shape
+    sizYest = yest.shape
     
+    if min(sizY) > 2 and min(sizYest)>2: #spectograms
+        inputType = 'F'
+    else:
+        inputType = 'T'
     
-    if fBands <= 1: #time domain
-        SDR,NRR = np.zeros(tBands),np.zeros(tBands)
-        if norm_yest:
-            yest =[LA.norm(y)/LA.norm(yest) * el_yest for el_yest in yest]
-        if clip_yest:
-            lowerBound =[(1+ 10**(-beta/20)) * ely for ely in y]
-            yest =[np.min([elyest, ellowerBound]) for elyest, ellowerBound in zip(yest,lowerBound)]
+    # retrieve params' values
+    keys = kwargs.keys()
+    
+    if 'tBands' in keys:
+        tBands = kwargs.pop('tBands', '')
+    else:
+        tBands = 1
         
-        if tBands > 0: #cut y into pieces
-            linscale= np.round(np.linspace(0,y.shape[0],tBands))
+    if 'fBands' in keys:
+        fBands = kwargs.pop('fBands', '')
+    else:
+        fBands=1
+     
+        
+    # cqse: we wanr to calculate the metrics from the T-F representations of y and yest   
+    if fBands > 1:   
+        if 'samplerate' in keys:
+            samplerate = kwargs.pop('samplerate','')
+        else:
+            samplerate = 44100 # or 10k # or display an error
+        
+        if 'n_fft' in keys:
+            n_fft = kwargs.pop('n_fft','')
+        else:
+            n_fft = 256 # 0-padded to 512
             
-        for n in range(tBands): 
+        if 'hop_length' in keys:
+            hop_length = kwargs.pop('hop_length','')
+        else:
+            hop_length = n_fft/2 
+        
+           
+        if inputType == 'T': # need to perform the STFT first
+            Yest = librosa.core.stft(yest, hop_length=hop_length, win_length=n_fft,window='hann')
+            Y    = librosa.core.stft(y,    hop_length=hop_length, win_length=n_fft,window='hann')
+                
+        elif inputType == 'F':
+            Yest = yest
+            Y    = y
             
-            # SIgnal 2 Distorsion Ratio:
-            diffTrue2Est= [a-b for a,b in zip( y[np.int(linscale[n]) : np.int(linscale[n+1])],\
-                                                yest[np.int(linscale[n]) : np.int(linscale[n+1])] )]
-            numSDR = np.mean(np.square(diffTrue2Est))    
-            denomSDR= np.mean(np.square(yest[np.int(linscale[n]) : np.int(linscale[n+1])]))
-            # formula uses inverse ratio inside the log, compared to the paper cited 
-            SDR[n]=10* np.log10(numSDR / denomSDR)
-            
-            # Noise Reduction Ratio:
-            numNR =np.mean(np.square( y[np.int(linscale[n]) : np.int(linscale[n+1])] ))
-            denomNR= np.mean(np.square( yest[np.int(linscale[n]) : np.int(linscale[n+1])] ))
-            NRR[n]= 10* np.log10(numNR / denomNR)
-        
-        
-    else: #fBands > 1, do it in the complex domain
-        
-        SDR,NRR = np.zeros(fBands,tBands),np.zeros(fBands,tBands)
-
-        n_fft = params['n_fft']
-        hop_length = params['hop_length']
-        samplerate = params['samplerate']
-        
-        ## compute stft
-        Yest= librosa.core.stft(yest, hop_length=hop_length, win_length=n_fft)
-        Y= librosa.core.stft(y, hop_length=hop_length, win_length=n_fft)
-          
         ## calculate the grid for the calculation of the metrics
-        # Frequence: just to get a log scale:
-        logscale = librosa.mel_frequencies(n_mels=fBands,fmin=0,fmax=samplerate/2)
-        #librosa.fft_frequencies(sr=22050, n_fft=subbands)
+        # Frequence: :
+        logscale = librosa.mel_frequencies(n_mels=fBands,fmin=0,fmax=samplerate/2) #just to get a log scale
+        #librosa.fft_frequencies(sr=22050, n_fft=subbands)        
         stepsF = np.floor(logscale/logscale[-1]* Y.shape[0])
         
         # Time:
         if tBands > 0:
-            stepsT = np.round(np.linspace(0,y.shape[1],tBands))
+            stepsT = np.round(np.linspace(0,Y.shape[1],tBands))
         else:
-            stepsT= np.array([0,y.shape[0]])
+            tBands = 1
+            stepsT = np.array([0,Y.shape[1]])
+    
+   
+    else: #if fBands <= 1: (and  inputType == 'T' or 'F') : time domain
+        fBands = 1
+        stepsF = np.array([0,1])
+        
+        Yest = yest
+        Y    = y
+        
+        if tBands > 0: #grid to cut y and yest into pieces
+            stepsT = np.round(np.linspace(0, y.shape[0], tBands))
+        else:
+            stepsT = np.array([0,sizY[0]])
             
-        #Ytemp=Y
-        for nf in range(fBands):
-            for nt in range(tBands):
-                #if norm_yest and clip_yest:
-                #Ytemp[nf][nt] = np.min([LA.norm(Y[nf][nt])/LA.norm(Yest[nf][nt]) * Yest[nf][nt], (1+ 10**(-beta/20) * Y[j][m] )])
+    
+    SDR, NRR = np.zeros(fBands,tBands), np.zeros(fBands,tBands)
+    
+    for nf in range(fBands):
+        for nt in range(tBands):
 
-                # SIgnal 2 Distorsion Ratio:
-                diffTrue2Est= [a-b for a,b in zip( \
-                        Y[ np.int(stepsF[n]) : np.int(stepsF[n+1]) ][ np.int(stepsT[n]) : np.int(stepsT[n+1]) ],\
-                        Yest[ np.int(stepsF[n]) : np.int(stepsF[n+1]) ][ np.int(stepsT[n]) : np.int(stepsT[n+1])]) ]
-                numSDR = np.mean(np.square(diffTrue2Est))    
-                denomSDR= np.mean( np.square( Yest[np.int(stepsF[n]):np.int(stepsF[n+1])][np.int(stepsT[n]):np.int(stepsT[n+1])]))
-                # formula uses inverse ratio inside the log, compared to the paper cited 
-                SDR[nf][nt]= 10*np.log10(numSDR / denomSDR)
+            yestSelec = Yest[ np.int(stepsF[nf]) : np.int(stepsF[nf+1]) , np.int(stepsT[nt]) : np.int(stepsT[nt+1])]
+            ySelec    = Y[    np.int(stepsF[nf]) : np.int(stepsF[nf+1]) , np.int(stepsT[nt]) : np.int(stepsT[nt+1]) ]
+            
+#            Normalisation?
+#            if norm_yest and clip_yest:
+#            Ytemp[nf][nt] = np.min([LA.norm(Y[nf][nt])/LA.norm(Yest[nf][nt]) * Yest[nf][nt], (1+ 10**(-beta/20) * Y[j][m] )])
+#            OR
+#            if norm_yest:
+#                yest =[LA.norm(y)/LA.norm(yest) * el_yest for el_yest in yest]
+#            if clip_yest:
+#                lowerBound =[(1+ 10**(-beta/20)) * ely for ely in y]
+#                yest =[np.min([elyest, ellowerBound]) for elyest, ellowerBound in zip(yest,lowerBound)]
+
+    
+            # SIgnal 2 Distorsion Ratio:
+            diffTrue2Est= [ a-b for a,b in zip(ySelec, yestSelec) ]
+                    
+            numSDR   = np.mean(diffTrue2Est @ diffTrue2Est.T)    
+            denomSDR = np.mean(yestSelec @ yestSelec.T)
+            # formula uses inverse ratio inside the log, compared to the paper cited 
+            SDR[nf][nt] = 10 * np.log10(numSDR / denomSDR)
+            
+            # Noise Reduction Ratio:
+            numNR   = np.mean(ySelec @ ySelec.T)
+            denomNR = denomSDR
+            NRR[nf][nt] = 10 * np.log10(numNR / denomNR)
+
+        
+    # Calculation of STOI
+    STOI = calc_STOI(y,yest,**kwargs)
                 
-                # Noise Reduction Ratio:
-                numNR =np.mean(np.square( y[np.int(linscale[n]) : np.int(linscale[n+1])] ))
-                denomNR= np.mean(np.square( yest[np.int(linscale[n]) : np.int(linscale[n+1])] ))
-                NRR[nf][nt]= 10* np.log10(numNR / denomNR) 
-    
-    
-    # calc Noise Reduction ratio NR
-    
-    output = {'Signal-To-Distorsion Ratio (SDR)' : SDR, 'Noise Reduction Ratio' : NRR}
+    output = {'Signal-To-Distorsion Ratio (SDR)' : SDR, 'Noise Reduction Ratio' : NRR, 'STOI' : STOI}
     return output
 
     
 
-
+def calc_STOI(y, yest, **kwargs):
+    keys =kwargs.keys()
+    
+    if 'norm_yest' in keys:
+        norm_yest=kwargs.pop('norm_yest', '')
+    else:
+        norm_yest=True
+        
+    if 'clip_yest' in keys:
+        clip_yest=kwargs.pop('clip_yest', '')
+    else:
+        clip_yest=True
+        
+    # Parameters
+    if 'beta' in keys:
+        beta=kwargs.pop('beta', '')
+    else:
+        beta = -15 #lower SDR bound
+    
+    if 'STOIsamplerate' in keys:
+        STOIsamplerate=kwargs.pop('STOIsamplerate', '')
+    else:
+        STOIsamplerate= 10000
+        
+        
+    nFrame= 256 #for a new STFT? 0-padding until 512
+    
+    if 'STOIdurFrame' in keys:
+        STOIdurFrame = kwargs.pop('STOIdurFrame','')
+    else:
+        STOIdurFrame = .386 # in sec, optimal for STOI according to the ref
+    STOInFrame= np.int(STOIdurFrame * STOIsamplerate)
+    
+    #### TODO
+    # stft of time-domain signals / can be done with specs but then includes uncertainties about the parameters' values used
+    # eliminate sequences without speech (energy < 40 dB)
+    # a one-third octave band analysis by grouping DFT-bins. In total 15 one-third octave bands > 150Hz and < 4.3kHz (center of the highest band)
+    # calculate T-F units = normSquareBin
+    # short-term segmentation
+    # normalise + clip yest
+    # correlation coeff
+    # average
+    STOI = -1
+    
+    return STOI
 
 
 
