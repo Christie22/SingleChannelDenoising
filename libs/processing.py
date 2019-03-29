dd391578ac67eb74045170cd80164d35383c84a8
Riccardo Miccini <rimiccini@yahoo.it>
2019-03-29 23:33:30 +0100

    fixes

--- libs/processing.py
+++ libs/processing.py
@@ -53,7 +53,7 @@ def s_to_power(s):
     return np.expand_dims(s_power, axis=2)
 
 def power_to_s(power, s_noisy=None):
-    s = np.sqrt(power)
+    s = np.sqrt(np.abs(power[...,0]))
     # TODO might require noisy signal as input for phase
     # add previously removed bin
     pad_shape = list(s.shape)
